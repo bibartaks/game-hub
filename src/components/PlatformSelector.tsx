5aca704bf64a689fddacd39a1d26529c66f14f08
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-05-18 12:12:33 +0600

    Build Platform Selector

---
+++ src/components/PlatformSelector.tsx
@@ -0,0 +1,24 @@
+import { Button, Menu, MenuButton, MenuList } from "@chakra-ui/react";
+import { BsChevronBarDown } from "react-icons/bs";
+import usePlatforms from "../hooks/usePlatforms";
+
+const PlatformSelector = () => {
+  const { data, error } = usePlatforms();
+
+  if (error) null;
+
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>
+        Platforms
+      </MenuButton>
+      <MenuList>
+        {data.map((platform) => (
+          <MenuList key={platform.id}>{platform.name}</MenuList>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default PlatformSelector;
